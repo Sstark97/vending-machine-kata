d42c3c68342668a7f317dc6de13183ff6f3a273e
Sstark97 <aitorscinfo@gmail.com>
2024-01-19 12:37:31 +0000

    :wrench: Add vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
         includeSource: ["src/**/*.ts"],
         globals: true,
-    }
+        alias: {
+            "@core":  new URL('./src/core', import.meta.url).pathname,
+            "@test": new URL('./src/test', import.meta.url).pathname,
+        }
+    },
 });
